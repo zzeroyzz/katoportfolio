faceec0be4e995a21ea316c580272cd9a7918237
kato hawkins <kristhahawkins@icloud.com>
2021-10-21 13:38:58 -0400

    removed unneeded field

--- src/components/section/Works.js
+++ src/components/section/Works.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Project from '../layouts/Project'
-import { projects, miscellaneous, section3Title, section4Title } from '../../profile'
+import { projects,  section3Title } from '../../profile'
 
 const Works = () => {
     return (
@@ -18,19 +18,7 @@ const Works = () => {
                 </div>
             </div>
 
-            <div className="third">
-                <>
-                    <div className="pp-head-line mx-auto text-center">
-                        <h1 id="Projects" className="red-line pp-head">{section4Title}</h1>
-                    </div>
-                </>
-                <div className="row">
-                    {miscellaneous && miscellaneous.map((x) => 
-                    <Project key={x.id} id={x.id} url={x.url} name={x.name} />
-                    )}
-                </div>
-            </div>
-        </>
+          </>      
     )
 }
 
