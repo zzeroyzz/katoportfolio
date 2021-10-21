c6b1f7a05a9af1be622212d15340ab4c76b15582
kato hawkins <kristhahawkins@icloud.com>
2021-10-21 13:40:02 -0400

    added new skills

--- src/profile.js
+++ src/profile.js
@@ -9,7 +9,7 @@ const animation = {
 };
 // Change your display name on tha landing display
 const header = {
-  name: "a Human",
+  name: "Kato Hernandez",
 };
 const background = {
   // Options: Snow or Particle
@@ -20,7 +20,7 @@ const background = {
 const section2title = "About Me";
 const about = {
   paragraph:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. ",
+    "My name is Kristha Hernandez, but I go by Kato. I specialize in full stack web developing with an eye for design concepts. I create beautiful simplistic websites that creatively solve a problem to provides services, and apps around the world.",
 };
 // Edit your skill and the percentage you know about it
 // To Add a skill, copy any one below and paste it after the last comma
@@ -57,25 +57,65 @@ const skillsBar = [
     faClass: "fab fa-python",
   },
   {
-    name: "Java",
+    name: "Docker",
     // svg: '',
-    faClass: "fab fa-java",
+    faClass: "fab fa-docker" ,
   },
   {
-    name: "PHP",
+    name: "React",
     // svg: '',
-    faClass: "fab fa-php",
+    faClass:"fab fa-react",
   },
   {
-    name: "Database",
+    name: "SQL",
     // svg: '',
     faClass: "fas fa-database",
   },
   {
-    name: "AWS",
+    name: "Cypress",
     // svg: '',
-    faClass: "fab fa-aws",
+    faClass: "fab fa-js",
   },
+  {
+    name: "Postman",
+    // svg: '',
+    faClass: "fas fa-database",
+  },
+  {
+    name: "Figma",
+    // svg: '',
+    faClass:"fab fa-figma",
+  },
+  {
+    name: "Express",
+    // svg: '',
+    faClass: "fab fa-react",
+  },
+  {
+    name: "Jest",
+    // svg: '',
+    faClass: "fab fa-react",
+  },
+  {
+    name: "Context API",
+    // svg: '',
+    faClass: "fab fa-react",
+  },
+  {
+    name: "Redux",
+    // svg: '',
+    faClass: "fab fa-react",
+  },
+  {
+    name: "Trello",
+    // svg: '',
+    faClass: "fab fa-trello",
+  },
+  {
+    name: "Algorithms",
+    // svg: '',
+    faClass: "fab fa-python",
+  }
 ];
 // Edit your projects, its name, your skills used to make it, and the url.
 // You can omit freely anything if you dont have it
@@ -85,70 +125,70 @@ const projects = [
   {
     // Add image in './styles/images.css' in #project1
     id: "project1",
-    name: "Project 1",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    name: "Nudge Budge Expense Tracker",
+    skills: ["React, Material-ui, JS,CSS"],
+    url: "https://nudgebudgeexpensetracker.netlify.app",
   },
-  {
-    // Add image in './styles/images.css' in #project2
-    id: "project2",
-    name: "Project 2",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
-  },
-  {
-    // Add image in './styles/images.css' in #project3
-    id: "project3",
-    name: "Project 3",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
-  },
-  {
-    // Add image in './styles/images.css' in #project4
-    id: "project4",
-    name: "Project 4",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
-  },
-  {
-    // Add image in './styles/images.css' in #project5
-    id: "project5",
-    name: "Project 5",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
-  },
-  {
-    // Add image in './styles/images.css' in #project6
-    id: "project6",
-    name: "Project 6",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
-  },
+  // {
+  //   // Add image in './styles/images.css' in #project2
+  //   id: "project2",
+  //   name: "Project 2",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
+  // {
+  //   // Add image in './styles/images.css' in #project3
+  //   id: "project3",
+  //   name: "Project 3",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
+  // {
+  //   // Add image in './styles/images.css' in #project4
+  //   id: "project4",
+  //   name: "Project 4",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
+  // {
+  //   // Add image in './styles/images.css' in #project5
+  //   id: "project5",
+  //   name: "Project 5",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
+  // {
+  //   // Add image in './styles/images.css' in #project6
+  //   id: "project6",
+  //   name: "Project 6",
+  //   skills: ["HTML, CSS, JS"],
+  //   url: "https://github.com/kaustubhai",
+  // },
 ];
 // Edit your Miscellaneous Activities, its name and the url.
 // You can omit freely anything if you dont have it
 // To Add a Activity, copy any one below and paste it after the last comma and increment the id's Miscellaneous number
-const section4Title = "Miscellaneous";
-const miscellaneous = [
-  {
-    // Add image in './styles/images.css' in #misc1
-    id: "misc1",
-    name: "Miscellaneous 1",
-    url: "https://github.com/kaustubhai",
-  },
-  {
-    // Add image in './styles/images.css' in #misc2
-    id: "misc2",
-    name: "Miscellaneous 2",
-    url: "https://github.com/kaustubhai",
-  },
-  {
-    // Add image in './styles/images.css' in #misc3
-    id: "misc3",
-    name: "Miscellaneous 3",
-    url: "https://github.com/kaustubhai",
-  },
-];
+// const section4Title = "Miscellaneous";
+// const miscellaneous = [
+//   {
+//     // Add image in './styles/images.css' in #misc1
+//     id: "misc1",
+//     name: "Miscellaneous 1",
+//     url: "https://github.com/kaustubhai",
+//   },
+//   {
+//     // Add image in './styles/images.css' in #misc2
+//     id: "misc2",
+//     name: "Miscellaneous 2",
+//     url: "https://github.com/kaustubhai",
+//   },
+//   {
+//     // Add image in './styles/images.css' in #misc3
+//     id: "misc3",
+//     name: "Miscellaneous 3",
+//     url: "https://github.com/kaustubhai",
+//   },
+// ];
 // Contact form text, and Formspree link(to send a submit contact through their API as in contact.js)
 // To get your own jotform link, go to https://formspree.io/
 // If you hacve the link already, paste it in the contactUrl below
@@ -177,11 +217,11 @@ export {
   about,
   skillsBar,
   projects,
-  miscellaneous,
+  // miscellaneous,
   contact,
   social,
   section2title,
   section3Title,
-  section4Title,
+  // section4Title,
   section5Title,
 };
